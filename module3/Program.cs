2b12edfada12f6152c52322aaa013fd3c8185723
Thomas Aschemann <thomas@metalixir.net>
2015-07-01 22:04:11 -0500

    Added set and echo methods

--- module3/Program.cs
+++ module3/Program.cs
@@ -10,6 +10,28 @@ namespace module3
     {
         static void Main(string[] args)
         {
+            getStudentInformation();
+            echoStudentDetails();
+        }
+
+        //Create method to get student information and assign it to a variable
+
+        //Next, create methods such as PrintStudentDetails(string first, string last, string birthday)
+        //that accepts the proper variables, and use an appropriate message to print the content to the console window.
+
+        static void getStudentInformation()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the student's date of birth: ");
+            string birthday = Console.ReadLine();
+        }
+
+        static void echoStudentDetails()
+        {
+            Console.WriteLine("{0} {1} was born on: {2}", first, last, birthday);
         }
     }
 }
