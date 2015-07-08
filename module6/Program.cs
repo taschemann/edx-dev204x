ed85e707189a55138c4c935a553f12e9b25731e4
Thomas Aschemann <thomas@metalixir.net>
2015-07-08 13:49:51 -0500

    Completed module 6

--- module6/Program.cs
+++ module6/Program.cs
@@ -10,14 +10,75 @@ namespace module6
     {
         static void Main(string[] args)
         {
-            //Student instantiation
-            student individual = new student();
-            //The int age is defined in the person class. This works because of inheritance.
-            individual.age = 10;
-            individual.height = 30;
-            individual.weight = 210;
+            #region Module 5 Code
+            //Instantiate a Course object called Programming with C#
+            course programmingWithCSharp = new course();
+            programmingWithCSharp.CourseName = "Programming in C#";
 
-            //Copy and paste module 5 code to other classes before continuing. Need class hierarchy.
+            //Instantiate three student objects and add them to the course object
+            programmingWithCSharp.Students = new student[3];
+            for (int i = 0; i < 3; i++)
+            {
+                programmingWithCSharp.Students[i] = new student();
+                student.StudentCount++;
+            }
+
+            student student1 = new student();
+            try
+            {
+                student1.TakeTest();
+            }
+            catch (NotImplementedException notImplemented)
+            {
+                Console.WriteLine(notImplemented.Message);
+            }
+
+            //Instantiate at least one Teacher object
+            teacher teacher1 = new teacher();
+
+            //Add that Teacher object to your Course object - only 1 instructor for this module
+            programmingWithCSharp.Instructor = new teacher[1] { teacher1 };
+
+            //Instantiate a Degree object
+            degree bachelorDegree = new degree();
+
+            //Add your Course object to the Degree object
+            bachelorDegree.Course = programmingWithCSharp;
+
+            //Instantiate a UProgram object called Information Technology
+            uprogram informationTechnology = new uprogram();
+
+            //Add the Degree object to the UProgram object
+            informationTechnology.Degrees = bachelorDegree;
+            //These are added for readability and testing in output
+            informationTechnology.ProgramName = "Information Technology";
+            informationTechnology.Degrees.DegreeName = "ISAT";
+
+            //Console Output
+            Console.WriteLine("The name of the Program is {0} and the Degree is {1}.", informationTechnology.ProgramName, informationTechnology.Degrees.DegreeName);
+            Console.WriteLine("The name of the Course is {0}.", informationTechnology.Degrees.Course.CourseName);
+            Console.WriteLine("The number of students enrolled is {0}", student.StudentCount);
+
+            #endregion
+
+            #region Challenge Code
+            //Create an instance of a Person object in code
+            person person1 = new person();
+            //Create an instance of a Student object in code
+            student student2 = new student();
+            //Assign the Student object to the Person object
+            person1 = student2;
+            //Access the properties of the Person instance you created
+            person1.FirstName = "Thomas";
+            
+            //This doesn't work because person1 is an instance of the person class
+            //When we assign an inherited class back to a parent object, we lose the
+            //functionality of the inherited class.
+                
+            //person1.TakeTest();
+
+            Console.WriteLine(person1.FirstName);
+            #endregion
         }
     }
 }
--- module6/student.cs
+++ module6/student.cs
@@ -8,6 +8,15 @@ namespace module6
 {
     class student : person
     {
+        //Student Count Variable
+        private static int studentCount;
+
+        public static int StudentCount
+        {
+            get { return studentCount; }
+            set { studentCount = value; }
+        }
+
         public int TakeTest()
         {
             throw new NotImplementedException();
