8b8a435a4846729648f1b973e211c532ab589082
Thomas Aschemann <thomas@metalixir.net>
2015-07-09 16:25:28 -0500

    Made code readable for peer review

--- module1/Program.cs
+++ module1/Program.cs
@@ -11,15 +11,36 @@ namespace assignment1
         static void Main(string[] args)
         {
             string firstName;
+            firstName = "John";
             string lastName;
+            lastName = "Johnson";
             string birthDate;
+            birthDate = "December 1, 1990";
             string addressOne;
+            addressOne = "123 Your Driveway";
             string addressTwo;
+            addressTwo = null;
             string city;
+            city = "St. Louis";
             string state;
+            state = "MO";
             int zipCode;
+            zipCode = 63101;
             string country;
-            
+            country = "USA";
+
+            Console.WriteLine("First Name: \t\n" + firstName);
+            Console.WriteLine("Last Name: \t" + lastName);
+            Console.WriteLine("Birthday: \t" + birthDate);
+            Console.WriteLine("Address One: \t" + addressOne);
+            Console.WriteLine("Address Two: \t" + addressTwo);
+            Console.WriteLine("City: \t" + city);
+            Console.WriteLine("State: \t" + state);
+            Console.WriteLine("ZIP: \t" + zipCode);
+            Console.WriteLine("Country: \t" + country);
+
+            /*
+             * Challenge Code
             Console.WriteLine("Please enter your first name: ");
             firstName = Console.ReadLine();
             Console.WriteLine("Please enter your last name: ");
@@ -38,16 +59,7 @@ namespace assignment1
             zipCode = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter your Country: ");
             country = Console.ReadLine();
-
-            Console.WriteLine("First Name: \t" + firstName);
-            Console.WriteLine("Last Name: \t" + lastName);
-            Console.WriteLine("Birthday: \t" + birthDate);
-            Console.WriteLine("Address One: \t" + addressOne);
-            Console.WriteLine("Address Two: \t" + addressTwo);
-            Console.WriteLine("City: \t" + city);
-            Console.WriteLine("State: \t" + state);
-            Console.WriteLine("ZIP: \t");
-            Console.WriteLine("Country: \t" + country);
+             * */
         }
     }
 }
