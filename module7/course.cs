ce3932a1cf8772f0064804a051b48e4066aa20ba
Thomas Aschemann <thomas@metalixir.net>
2015-07-09 14:57:18 -0500

    Added remaining files for module 7

---
+++ module7/course.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module7
+{
+    class course
+    {
+        #region Variables and Properties
+
+        //Course Name
+        private string courseName;
+
+        public string CourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+
+        //Credits Received
+        private int credits;
+
+        public int Credits
+        {
+            get { return credits; }
+            set { credits = value; }
+        }
+
+        //Duration of class in weeks
+        private int lengthInWeeks;
+
+        public int LengthInWeeks
+        {
+            get { return lengthInWeeks; }
+            set { lengthInWeeks = value; }
+        }
+
+        //Instructor
+        private teacher[] instructor;
+
+        public teacher[] Instructor
+        {
+            get { return instructor; }
+            set { instructor = value; }
+        }
+
+        //Student ArrayList
+        private ArrayList students = new ArrayList();
+
+        public ArrayList Students
+        {
+            get { return students; }
+            set { students = value; }
+        }
+
+        //Basically the same as the MSDN code in the student class with some slight modifications
+        public void ListStudent(ArrayList listObj)
+        {
+            foreach (student stud in listObj)
+            {
+                Console.WriteLine("First name of student{0} is " + stud.FirstName);
+                Console.WriteLine("Last name of student{0} is " + stud.LastName);
+            }
+            //Print blank line
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
---
+++ module7/degree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module7
+{
+    class degree
+    {
+        #region Variables and Properties
+        //Degree Name
+        private string degreeName;
+
+        public string DegreeName
+        {
+            get { return degreeName; }
+            set { degreeName = value; }
+        }
+
+        //Credits Required
+        private int creditsRequired;
+
+        public int CreditsRequired
+        {
+            get { return creditsRequired; }
+            set { creditsRequired = value; }
+        }
+
+        //Courses Required for Degree
+        private course course;
+
+        internal course Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        #endregion
+    }
+}
