fde9c3fd8ac5718fafcb5aa405a9469051a49292
Thomas Aschemann <thomas@metalixir.net>
2015-08-01 13:41:52 -0500

    Actually added student class

---
+++ module9/Mod_9_Homework/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student
+    {
+        //First Name
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        //Last Name
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        //Birth Date
+        private string birthDate;
+
+        public string BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+    }
+}
